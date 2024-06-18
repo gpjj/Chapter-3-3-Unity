bbf63514a4327277ddd0e5fc1cab61038b6128db
gpjj <iwbgp019@naver.com>
2024-06-18 11:25:21 +0900

    플레이어 무브먼트 구현

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector3 moveInput = value.Get<Vector3>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+}
